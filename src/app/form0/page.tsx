98e5285937780ef3eff13b89f071335c269033c2
nakayama hiroyuki <yuki@nakayamanoMacBook-Pro.local>
2025-07-03 17:02:39 +0900

    form improvement, sigin entry

--- src/app/form0/page.tsx
+++ src/app/form0/page.tsx
@@ -27,92 +27,92 @@ export default function Home() {
     const onSubmit = (data: any) => {
         console.log(data);
     };
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.field}>
-            <input type="text"  placeholder="名前" {...register('name')}/>
-            {errors.name && <p className={styles.error}>{errors.name.message}</p>}
-            </div>
-            <div className={styles.field}>
-                <input type="email" placeholder="Email" {...register('email')}/>
-                {errors.email && <p className={styles.error}>{errors.email.message}</p>}
-            </div>
-            <div className={styles.field}>
-                <input type="text" placeholder="住所" {...register('address')}/>
-                {errors.address && <p className={styles.error}>{errors.address.message}</p>}
-            </div>
-            <div className={`${styles.field} ${styles.radio_group}`}>
-                <div>
-                    <p>性別</p>
+    return (
+        <div className={styles.page}>
+        <main className={styles.main}>
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                <div className={styles.field}>
+                <input type="text"  placeholder="名前" {...register('name')}/>
+                {errors.name && <p className={styles.error}>{errors.name.message}</p>}
                 </div>
-                <div>
-                    <input type="radio" value="female" id="lady" {...register('gender')}/>
-                    <label htmlFor="#lady" >女性</label>
+                <div className={styles.field}>
+                    <input type="email" placeholder="Email" {...register('email')}/>
+                    {errors.email && <p className={styles.error}>{errors.email.message}</p>}
                 </div>
-                <div>
-                    <input type="radio" value="male" id="gentleman" {...register('gender')}/>
-                    <label  htmlFor="#gentleman">男性</label>
+                <div className={styles.field}>
+                    <input type="text" placeholder="住所" {...register('address')}/>
+                    {errors.address && <p className={styles.error}>{errors.address.message}</p>}
                 </div>
-                {errors.gender && <p className={styles.error}>{errors.gender.message}</p>}
-            </div>
-            <div className={`${styles.field} ${styles.checkbox} ${styles.flex_center}`}>
-                <div>
-                    <input type="checkbox" id="agree"  {...register('agree')}/>
-                    <label htmlFor="agree">利用規約に同意する</label>
-                </div>                
-            </div>
-            {errors.agree && <p className={styles.error}>{errors.agree.message}</p>}
-            <div className={styles.field}>
-                <button type="submit">送信</button>
-            </div>
-        </form>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+                <div className={`${styles.field} ${styles.radio_group}`}>
+                    <div>
+                        <p>性別</p>
+                    </div>
+                    <div>
+                        <input type="radio" value="female" id="lady" {...register('gender')}/>
+                        <label htmlFor="#lady" >女性</label>
+                    </div>
+                    <div>
+                        <input type="radio" value="male" id="gentleman" {...register('gender')}/>
+                        <label  htmlFor="#gentleman">男性</label>
+                    </div>
+                    {errors.gender && <p className={styles.error}>{errors.gender.message}</p>}
+                </div>
+                <div className={`${styles.field} ${styles.checkbox} ${styles.flex_center}`}>
+                    <div>
+                        <input type="checkbox" id="agree"  {...register('agree')}/>
+                        <label htmlFor="agree">利用規約に同意する</label>
+                    </div>                
+                </div>
+                {errors.agree && <p className={styles.error}>{errors.agree.message}</p>}
+                <div className={styles.field}>
+                    <button type="submit">送信</button>
+                </div>
+            </form>
+        </main>
+        <footer className={styles.footer}>
+            <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <Image
+                aria-hidden
+                src="/file.svg"
+                alt="File icon"
+                width={16}
+                height={16}
+            />
+            Learn
+            </a>
+            <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <Image
+                aria-hidden
+                src="/window.svg"
+                alt="Window icon"
+                width={16}
+                height={16}
+            />
+            Examples
+            </a>
+            <a
+            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            <Image
+                aria-hidden
+                src="/globe.svg"
+                alt="Globe icon"
+                width={16}
+                height={16}
+            />
+            Go to nextjs.org →
+            </a>
+        </footer>
+        </div>
+    );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import styles from "./page.module.css";
+export default function LoginPage(){
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
